005811a937a6fc714b1fda81f05669add9cf3073
Antoine F Moevus <2613844+antmoev@users.noreply.github.com>
2025-05-13 18:45:11 -0400

    Add Question01.vue Component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
-<script></script>
+<script setup>
+import Question01 from "@/components/Question01.vue";
+</script>
 
 <template>
 <h1> Introduction à Vue.js pour le de web app</h1>
+
+  <h2>Liste des exemples</h2>
+  <div class="exemple">
+  <h3>Question 01</h3>
+  <Question01/>
+  </div>
+
 </template>
 
 <style>
@@ -9,5 +18,10 @@
 body {
   background-color: #a3a3a3;
 }
+.exemple {
+  border: 8px solid #000000;
+  margin: 20px auto;
+  padding: 10px;
 
+}
 </style>
---
+++ src/components/Question01.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+<h1>Question 01</h1>
+
+  <p>Quel est la couleur du ciel ?</p>
+  <ul>
+    <li>bleu</li>
+    <li>transparent</li>
+    <li>rose</li>
+    <li>aucune de ces réponses</li>
+  </ul>
+</template>
+
+<style scoped>
+
+</style>
